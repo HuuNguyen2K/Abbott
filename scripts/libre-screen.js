905c33cf8c4658af2b0546f54b58be0e56b9b470
bang <phambang.dev@gmail.com>
2022-03-09 14:30:23 +0700

    feat: add config slide libre screen

--- scripts/libre-screen.js
+++ scripts/libre-screen.js
@@ -1,53 +1,39 @@
-
-
 const btnLibreLeft = document.querySelector('.page-libre__btn-left')
-const btnLibreCenter =  document.querySelector('.page-libre__btn-center')
-const btnLibreRight =  document.querySelector('.page-libre__btn-right')
+const btnLibreCenter = document.querySelector('.page-libre__btn-center')
+const btnLibreRight = document.querySelector('.page-libre__btn-right')
 const elEventLibrePopupLeft = document.getElementById('event-libre-left')
 const elEventLibrePopupCenter = document.getElementById('event-libre-center')
 const elEventLibrePopupRight = document.getElementById('event-libre-right')
-console.log(elEventLibrePopupLeft)
-console.log(btnLibreLeft)
 
-showPopup = (event) => {
+const showPopup = (event) => {
     event.preventDefault();
     let showEventPopup = event.target.getAttribute('data-toggle');
-    const elEventLibrePopup =document.getElementById(showEventPopup)
-    console.log(elEventLibrePopup)
-    // elEventLibrePopup.classList.toggle('open',)
-    elEventLibrePopup.classList.add('open','overlay')
+    let elEventLibrePopup = document.getElementById(showEventPopup)
+    elEventLibrePopup.classList.add('open', 'overlay')
 }
-hidePopup = (event) => {
+
+const hidePopup = (event) => {
     event.preventDefault();
-    console.log(event)
-    // let showEventPopup = event.target.getAttribute('data-toggle');
-    // const elEventLibrePopup =document.getElementById(showEventPopup)
-    // console.log(elEventLibrePopup)
-    // elEventLibrePopup.classList.toggle('open',)
     event.target.classList.remove('open', 'overlay')
 }
 
 btnLibreLeft.addEventListener('click', showPopup)
 
-btnLibreCenter.addEventListener('click',  showPopup)
+btnLibreCenter.addEventListener('click', showPopup)
 
-btnLibreRight.addEventListener('click',  showPopup)
+btnLibreRight.addEventListener('click', showPopup)
 
 elEventLibrePopupLeft.addEventListener('click', hidePopup)
 
-elEventLibrePopupCenter.addEventListener('click',  hidePopup)
+elEventLibrePopupCenter.addEventListener('click', hidePopup)
 
-elEventLibrePopupRight.addEventListener('click',  hidePopup)
+elEventLibrePopupRight.addEventListener('click', hidePopup)
 
+// Slider
 const swiper = new Swiper('.swiper', {
     centeredSlides: true,
-    autoplay: {
-        delay: 2500
-    },
-
-    // If we need pagination
-    // pagination: {
-    //     el: '.swiper-pagination',
+    // autoplay: {
+    //     delay: 2500
     // },
 
     // Navigation arrows
