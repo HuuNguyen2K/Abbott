66a8c74c80e673a24d93cf71363d45162b212b31
Tinh Nguyen <tinh.nguyen@kegmil.com>
2022-03-13 10:07:57 +0700

    warning time

--- scripts/common.js
+++ scripts/common.js
@@ -1,11 +1,11 @@
-/**
- * Expect object: {
- *                  render_name: '...selected wanna render',
- *                  command: 'open' || 'close',
- *                  active_class: '...class name',
- *                  without_ele: '...tag name' || null
- *                 }
- */
+// /**
+//  * Expect object: {
+//  *                  render_name: '...selected wanna render',
+//  *                  command: 'open' || 'close',
+//  *                  active_class: '...class name',
+//  *                  without_ele: '...tag name' || null
+//  *                 }
+//  */
 const handleToggleByIssue = () => {
   const elementControl = document.querySelectorAll('[data-control]')
 
@@ -22,6 +22,56 @@ const handleToggleByIssue = () => {
     })
   })
 };
+
+const { useEffect, useState, useRef } = React;
+
+const WarningTimeOut = () => {
+  const [isWarning, setWarning] = useState(false);
+  const [isSeminarStart, setSeminarStart] = useState(false);
+  const endTimeRef = useRef(moment().add(1, 'minutes'));
+  const startTime = endTimeRef.current.clone().subtract(2, 'minutes');
+
+  const checkTime = () => {
+    const now = moment();
+    return now.isBetween(startTime, endTimeRef.current);
+  }
+
+  const checkSeminarStart = () => {
+    const now = moment();
+    return now.isSameOrAfter(endTimeRef.current);
+  }
+
+  useEffect(() => {
+    setInterval(() => {
+      setWarning(checkTime());
+      setSeminarStart(checkSeminarStart());
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    if (isWarning) alert('Còn 5, 10 phút nữa hết giờ trải nghiệm!');
+  }, [isWarning]);
+
+  useEffect(() => {
+    if (isSeminarStart) alert('Đã đến giờ hội thảo!');
+  }, [isSeminarStart]);
+
+  return (
+      <div>
+        {/*{*/}
+        {/*  isWarning && (*/}
+        {/*    <div style={{ color: 'red' }}>*/}
+        {/*      Còn 5, 10 phút nữa hết giờ trải nghiệm!*/}
+        {/*    </div>*/}
+        {/*  )*/}
+        {/*}*/}
+      </div>
+  );
+}
+
+const domContainer = document.querySelector('#warning-time-out');
+ReactDOM.render(<WarningTimeOut/>, domContainer);
+
 
 window.addEventListener('load', () => {
   handleToggleByIssue()
