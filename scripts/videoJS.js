a9334c755339e5c1605b87ea02815185e31720ce
Tinh Nguyen <tinh.nguyen@kegmil.com>
2022-03-13 17:11:03 +0700

    add videoJs lib

---
+++ scripts/videoJS.js
@@ -0,0 +1,60 @@
+const VideoJS = ( props ) => {
+
+    const [ended, setEnded] = useState(false);
+    const videoRef = React.useRef(null);
+    const playerRef = React.useRef(null);
+    const { src, options, onReady, onEnded } = props;
+
+    const defaultOptions = {
+        autoplay: true,
+        controls: true,
+        responsive: true,
+        fluid: true,
+        muted: true,
+        sources: [{
+            src,
+            type: 'application/x-mpegURL'
+        }]
+    };
+
+    React.useEffect(() => {
+        // make sure Video.js player is only initialized once
+        if (!playerRef.current) {
+            const videoElement = videoRef.current;
+            if (!videoElement) return;
+
+            const player = playerRef.current = videojs(videoElement, {...defaultOptions, ...options}, function (){
+                // console.log("player is ready");
+                onReady && onReady(player);
+                this.on('ended', () => { setEnded(true); onEnded && onEnded()});
+            });
+        } else {
+            // you can update player here [update player through props]
+            const video = videoRef.current;
+            video.muted = options.muted;
+            // const player = playerRef.current;
+            // player.autoplay(options.autoplay);
+            // player.src(options.sources);
+        }
+    }, [options, videoRef]);
+
+    // Dispose the Video.js player when the functional component unmounts
+    React.useEffect(() => {
+        const player = playerRef.current;
+
+        return () => {
+            if (player) {
+                player.dispose();
+                playerRef.current = null;
+            }
+        };
+    }, [playerRef]);
+
+    if (ended) return null;
+
+    return (
+        <div data-vjs-player>
+            <video ref={videoRef} className="video-js vjs-big-play-centered" />
+        </div>
+    );
+}
