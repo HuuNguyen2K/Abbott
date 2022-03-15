05ea385383bcfb8b5ee02e47ecfe93514f59e4d2
Tinh Nguyen <tinh.nguyen@kegmil.com>
2022-03-15 22:57:59 +0700

    mobile detect and fix menu bug

---
+++ scripts/mobileDetect.js
@@ -0,0 +1,14 @@
+const handleDevice = () => {
+    var md = new MobileDetect(window.navigator.userAgent);
+    if (md.mobile()) {
+        if(window.innerWidth < window.innerHeight) {
+            alert('Vui lòng xoay ngang!');
+        }
+    } else clearTimeout(handleCheck);
+};
+
+handleDevice();
+
+const handleCheck = setInterval(() => {
+    handleDevice();
+}, 10000);
