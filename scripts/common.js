99466e508f06b8ed65c9987b88be0c9d4a13ec1f
Chain <ntrungtn@gmail.com>
2022-03-16 14:54:44 +0700

    fix: await dom onload for file common

--- scripts/common.js
+++ scripts/common.js
@@ -1,28 +1,24 @@
-// /**
-//  * Expect object: {
-//  *                  render_name: '...selected wanna render',
-//  *                  command: 'open' || 'close',
-//  *                  active_class: '...class name',
-//  *                  without_ele: '...tag name' || null
-//  *                 }
-//  */
-// const handleToggleByIssue = () => {
-//   const elementControl = document.querySelectorAll('[data-control]')
-//
-//   elementControl.length && elementControl.forEach(ele => {
-//     ele.addEventListener('click', (evt) => {
-//       const objControl = JSON.parse(evt.target.dataset['control'])
-//       if (Object.keys(objControl).length) {
-//         const renderEle = document.querySelector(objControl.render_name)
-//         // BLock action user click with assign element
-//         if (objControl.without_ele && evt.target.localName === objControl.without_ele) return
-//         // Force check class exists
-//         renderEle.classList.toggle(objControl.active_class, objControl.command === 'open')
-//       }
-//     })
-//   })
-// };
-//
-// window.addEventListener('load', () => {
-//   handleToggleByIssue()
-// });
+/**
+ * Expect object: {
+ *                  render_name: '...selected wanna render',
+ *                  command: 'open' || 'close',
+ *                  active_class: '...class name',
+ *                  without_ele: '...tag name' || null
+ *                 }
+ */
+const handleToggleByIssue = () => {
+  const elementControl = document.querySelectorAll('[data-control]')
+
+  elementControl.length && elementControl.forEach(ele => {
+    ele.addEventListener('click', (evt) => {
+      const objControl = JSON.parse(evt.target.dataset['control'])
+      if (Object.keys(objControl).length) {
+        const renderEle = document.querySelector(objControl.render_name)
+        // BLock action user click with assign element
+        if (objControl.without_ele && evt.target.localName === objControl.without_ele) return
+        // Force check class exists
+        renderEle.classList.toggle(objControl.active_class, objControl.command === 'open')
+      }
+    })
+  })
+};
--- scripts/modal-qr.js
+++ scripts/modal-qr.js
@@ -14,6 +14,7 @@ const ModalQR = (props) => {
   }
 
   useEffect(() => {
+    handleToggleByIssue()
   }, []);
 
   return (
