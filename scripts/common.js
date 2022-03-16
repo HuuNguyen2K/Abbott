2a7cbc03e1e3e4df7635a0b9a65a00e01f037ac9
Chain <ntrungtn@gmail.com>
2022-03-16 23:27:09 +0700

    feat: reset iframe when closed modal, additional modal show faq

--- scripts/common.js
+++ scripts/common.js
@@ -16,9 +16,30 @@ const handleToggleByIssue = () => {
         const renderEle = document.querySelector(objControl.render_name)
         // BLock action user click with assign element
         if (objControl.without_ele && evt.target.localName === objControl.without_ele) return
+        // Stop game from playing
+        objControl.iframe && stopIframeVideo(renderEle.querySelector('.iframe-game'))
         // Force check class exists
         renderEle.classList.toggle(objControl.active_class, objControl.command === 'open')
       }
     })
   })
 };
+
+/**
+ * Stop iframe video
+ *
+ * @param {Node} iframe
+ */
+const stopIframeVideo = (iframe) => {
+  const url = iframe.getAttribute('src');
+  const urlCache = sessionStorage.getItem('url_game')
+  // Save url to storage
+  sessionStorage.setItem('url_game', url)
+  iframe.setAttribute('src', '');
+  // Restore url game for iframe
+  // Use 'abc' because wanna completely remove the dom inside the iframe
+  setTimeout(() => {
+    iframe.setAttribute('src', urlCache);
+    sessionStorage.removeItem('url_game')
+  }, 50)
+}
--- scripts/modal-qr.js
+++ scripts/modal-qr.js
@@ -13,12 +13,25 @@ const ModalQR = (props) => {
     // a.click();
   }
 
+  const handleOpenModalFaq = () => {
+    const modalFaq = document.querySelector('#modal-faq')
+    modalFaq && modalFaq.classList.add('modal-common-visible')
+  }
+
   useEffect(() => {
+    const closeModalFaq = document.querySelector('#close-modal-faq')
+    const modalFaq = document.querySelector('#modal-faq')
+    closeModalFaq && closeModalFaq.addEventListener('click', _ => {
+      modalFaq.classList.remove('modal-common-visible')
+    })
+
     handleToggleByIssue()
+
+    return () => closeModalFaq.removeEventListener('click', _ => {})
   }, []);
 
   return (
-      <div className="modal-qr">
+      <div className="modal-qr modal-qr-visible">
         <div className="qr-container">
           <h3 className="qr-title">Mời Quý Chuyên viên y tế <br/>chọn các tính năng bên dưới</h3>
           <img className="qr-code" src={ src } alt="qr_code"/>
@@ -30,9 +43,11 @@ const ModalQR = (props) => {
                    render_name: '.sec-iframe',
                    command: 'open',
                    active_class: 'open-iframe',
-                   without_ele: null
+                   without_ele: null,
+                   iframe: true
                  }) } />
-            <div className="qr-button faq"/>
+            <div className="qr-button faq"
+                 onClick={handleOpenModalFaq}/>
           </div>
         </div>
       </div>
