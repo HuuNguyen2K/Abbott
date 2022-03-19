aef4aab41c223ad9d19bbb35ce1e834c6acdf64d
Tinh Nguyen <tinh.nguyen@kegmil.com>
2022-03-19 14:20:12 +0700

    prevent orientation

--- scripts/mobileDetect.js
+++ scripts/mobileDetect.js
@@ -1,16 +1,29 @@
-const handleCheck = setInterval(() => {
-    handleDevice();
-}, 10000);
+// const handleCheck = setInterval(() => {
+//     handleDevice();
+// }, 10000);
 
-const handleDevice = () => {
+// const handleDevice = () => {
+//     var md = new MobileDetect(window.navigator.userAgent);
+//     if (md.mobile()) {
+//         if(window.innerWidth < window.innerHeight) {
+//             alert('Vui lòng xoay ngang!');
+//         }
+//     } else clearTimeout(handleCheck);
+// };
+
+const handlePreventOrientation = () => {
     var md = new MobileDetect(window.navigator.userAgent);
     if (md.mobile()) {
-        if(window.innerWidth < window.innerHeight) {
-            alert('Vui lòng xoay ngang!');
-        }
-    } else clearTimeout(handleCheck);
-};
+        new PreventOrientation({
+            text: 'Vui lòng xoay ngang!',
+            background: 'white',
+            fontSize: '20px'
+        }).preventOrientationToAngle(0);
+    }
+}
 
 window.addEventListener('load', () => {
-    handleDevice();
+    // handleDevice();
+    handlePreventOrientation();
 });
+
